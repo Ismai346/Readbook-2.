26f4a4ee2986e87fbb37aa35056e4ba9bb4945e4
Ismail Pedro <desmaelpedro@gmail.com>
2025-07-20 06:14:31 +0200

    Hello World

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("spck editor teste")
+
+
+
+
+<script>
+  document.querySelector('.enviar').addEventListener('click', function(event) {
+    event.preventDefault(); // Evita que o formulário seja enviado automaticamente
+    alert('Formulário enviado!');
+  });
+</script>
